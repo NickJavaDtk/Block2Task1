cd81115762812187a4aa381e7c91f1f456c66070
Nickolay Petrushin <nick.mflj@gmail.com>
2022-05-23 23:11:04 +0300

    for verification

--- src/main/java/ru/netology/ArrayOne.java
+++ src/main/java/ru/netology/ArrayOne.java
@@ -1,35 +1,40 @@
 package ru.netology;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class ArrayOne {
 
-
     public static void main(String[] args) {
         int[] prices = {90, 120, 95, 420, 2600, 150};
-        String volume = "литр";
-        String weigth = "килограмм";
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("Молоко") || args[i].equals("Ряженка")) {
-                System.out.println(i + 1 + ". " + args[i] + " по цене " + prices[i] + " рублей за " + volume);
-
-            } else {
-                System.out.println(i + 1 + ". " + args[i] + " по цене " + prices[i] + " рублей за " + weigth);
-            }
-
-        }
-        int[] purchase = new int[args.length];
+        String[] products = {"Молоко", "Творог", "Ряженка", "Масло сливочное", "Сыр с плесенью",
+                "Сырок плавленный \"Дружба\""};
+//        String volume = "литр";
+//        String weigth = "килограмм";
+//        for (int i = 0; i < products.length; i++) {
+//            if (products[i].equals("Молоко") || products[i].equals("Ряженка")) {
+//                System.out.println(i + 1 + ". " + products[i] + " по цене " + prices[i] + " рублей за " + volume);
+//
+//            } else {
+//                System.out.println(i + 1 + ". " + products[i] + " по цене " + prices[i] + " рублей за " + weigth);
+//            }
+//
+//        }
+        int[] purchase = new int[products.length];
         int count = 0;
+        int countWhile = 0;
         int productNumber = 0;
         int productCount = 0;
         while (true) {
+            countWhile++;
             System.out.println("Введите товар и количество или введите \"end\"");
-            //String input = forMainScanner(inputStreamReader);
-            String input = forMainScanner(inputStreamReader);
+            String input = forMainScanner(args[0]);
             if (input.equals("end")) {
-                System.out.println("Наименование товара   Количество  Цена  Общая стоимость");
+                //System.out.println("Наименование товара   Количество  Цена  Общая стоимость");
                 for (int i = 0; i < purchase.length; i++) {
                     if (purchase[i] != 0) {
                         System.out.println(args[i] + " " + purchase[i] + " " + prices[i] + " " + purchase[i] * prices[i]);
@@ -44,6 +49,10 @@ public class ArrayOne {
             purchase[productNumber - 1] += productCount;
 
             count += prices[productNumber - 1] * productCount;
+            if (countWhile == 1) {
+                System.out.println("Итого " + count);
+                break;
+            }
         }
 
 
@@ -67,21 +76,27 @@ public class ArrayOne {
 
     }
 
-    public static String[] forMainArgs() {
-        String[] products = {"Молоко", "Творог", "Ряженка", "Масло сливочное", "Сыр с плесенью",
-                "Сырок плавленный \"Дружба\""};
-        return products;
-    }
-
-    public static String forMainScanner(InputStreamReader inputStreamReader) {
-        String s = "";
-        Scanner scanner = new Scanner(inputStreamReader);
-        try {
-            s = scanner.nextLine( );
-        } catch (IllegalArgumentException | NullPointerException | NoSuchElementException e) {
-            e.printStackTrace( );
+    public static String forMainScanner(String input) {
+        String[] s = input.split(" ");
+        String result = null;
+        String s1 = "";
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].equals("end")) {
+                return s[i];
+            } else {
+                try (InputStreamReader is = new InputStreamReader(new ByteArrayInputStream(s[i].getBytes(StandardCharsets.UTF_8)));
+                     Scanner scanner = new Scanner(is)) {
+                    s1 += scanner.nextLine( );
+                    if (result == null) {
+                        s1 += " ";
+                        result = s1;
+                    }
+                } catch (IOException | IllegalArgumentException | NullPointerException | NoSuchElementException e) {
+                    e.printStackTrace( );
+                }
+            }
         }
-        return s;
+        return s1;
     }
 
 
--- src/test/java/ru/netology/ArrayOneTest.java
+++ src/test/java/ru/netology/ArrayOneTest.java
@@ -3,9 +3,11 @@ package ru.netology;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.NoSuchElementException;
 import java.util.function.BooleanSupplier;
 
@@ -14,25 +16,29 @@ public class ArrayOneTest {
 
     @Test
     public void userError() {
-        InputStreamReader isNull = new InputStreamReader(System.in);
-        // тут в значение isNull я планировал передавать с консоли не цифру
-        Assertions.assertThrows(NumberFormatException.class, () -> ArrayOne.main(ArrayOne.forMainArgs( )));
+        String[] s = {"12 три"};
+
+        Assertions.assertThrows(NumberFormatException.class, () -> ArrayOne.main(s));
     }
 
 
     @Test
     public void invalidInput() {
-        InputStreamReader isDigital = new InputStreamReader(System.in);
-        // тут в значение isDigital я планировал передавать с консоли только одно значение
-        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> ArrayOne.main(ArrayOne.forMainArgs( )));
+        String[] s = {"12 "};
+
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> ArrayOne.main(s));
     }
 
 
     @Test
-    public void inappropriateValue() {
-        InputStreamReader isInvalid = new InputStreamReader(System.in);
-        // тут в значение isInvalid я планировал передавать с консоли передать пустую строку
-        Assertions.assertThrows(NoSuchElementException.class, () -> ArrayOne.main(ArrayOne.forMainArgs( )));
+    public void checkValue() {
+        String s = "12 end";
+
+        String expected = "end";
+        String result = ArrayOne.forMainScanner(s);
+
+
+        Assertions.assertEquals(expected, result);
 
     }
 }
